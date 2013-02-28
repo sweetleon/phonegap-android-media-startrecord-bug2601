3cc5b978da430e88957c69d41463786f3ebba58d
lenny <git@sweetleon.com>
2013-02-28 08:03:27 -0800

    says it's recording but the file is always empty

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -55,49 +55,82 @@ function logToConsoleFunc(errorType) {
 	}
 }
 
-document.addEventListener("deviceready", function() {
-	var RECORD_FILENAME = 'listen.';
-	var TIME_WINDOW_MS = 100;
-
-	function shush() {
+var RECORD_FILENAME = 'listen.';
+var RECORD_TIME_MS = 500;
+var listener;
+function shush() {
 		var path = window.location.pathname;
-    path = path.substr( path, path.lastIndexOf('/') );
+		path = path.substr(path, path.lastIndexOf('/'));
 		new Media("file://" + path + "/audio/shush.mp3").play();
-	}
+}
+
+function showFileInfo(fileEntry) {
+	fileEntry.file(function(file) {
+		console.log("fileEntry.file " + file);
+
+setTimeout(function() {
+		var reader = new FileReader();
+		reader.onloadend = function(evt) {
+				console.log("file size " + file.size);
+				console.log("LISTENER: " + JSON.stringify(listener));
+        console.log("onloadend " + JSON.stringify(evt));
+        console.error(reader.result);
+    };
+		reader.readAsDataURL(file);
+}, 0);
+	}, logToConsoleFunc("recordFileEntry.file"));
+}
+
+document.addEventListener("deviceready", function() {
 	shush();
 
 	var recordToFile = RECORD_FILENAME;
+	var soundAnalyzerFn;
 	switch (device.platform) {
     case 'iOS':
-			recordToFile += 'amr';
+			recordToFile += 'wav';
+			soundAnalyzerFn = function() {
+				console.error("TODO: implement WAV analyzer")
+			}
       break;
 		default: // Android, Blackberry
-      recordToFile += 'wav';
+      recordToFile += 'amr';
+      soundAnalyzerFn = function() {
+        console.error("TODO: implement AMR analyzer")
+      }
 	}
 
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function gotFsForCreateBlank(fileSystem) {
-	  var RECORD_FILEPATH = fileSystem.root.fullPath + '/' + recordToFile;
-    console.log('creating ' + RECORD_FILEPATH);
-    fileSystem.root.getFile(recordToFile, {create: true, exclusive: false}, function() {
-        console.log("Initializing audio...");
-        navigator.audio = new Media(RECORD_FILEPATH, function(){},logToConsoleFunc('FAILED to create Media to record'));
-        console.log("Initializing audio...OK");
+	window.requestFileSystem(LocalFileSystem.TEMPORARY, 0, function gotFsForCreateBlank(fileSystem) {
+	  var recordFilePath = fileSystem.root.fullPath + '/' + recordToFile;
+    console.log('creating ' + recordFilePath);
+    fileSystem.root.getFile(recordToFile, {create: true, exclusive: false}, function gotFile(fileEntry) {
+        console.log("creating media");
+        listener = new Media(recordFilePath, function stoppedRecording(){
+          console.error("media done");
+          showFileInfo(fileEntry);
+
+        },logToConsoleFunc('FAILED to create Media to record'));
+
+				setTimeout(function() {recorder(fileEntry);}, 1000);
+
+        setInterval(function filePrinter() {showFileInfo(fileEntry)}, 1000);
     }, logToConsoleFunc("getFile (create)"));
 
-    function recorder() {
-        console.log("Starting recording...");
-        navigator.audio.startRecord();
-        console.log("Starting recording...OK");
+    function recorder(fileEntry) {
+				showFileInfo(fileEntry);
+
+        console.log("startRecord " + JSON.stringify(listener));
+        listener.startRecord();
 
         setTimeout(function stopRecording() {
-          console.log("Stopping recording...");
-          navigator.audio.stopRecord();
-          console.log("Stopping recording...OK");
-          recorder();
+          console.log("stopRecord " + JSON.stringify(listener));
+          listener.stopRecord();
 
-					console.log(document.getElementsByTagName("audio")[0].src)
-        }, TIME_WINDOW_MS);
+					document.getElementById('output').innerHTML += ".";
+
+//TODO          recorder(fileEntry);
+        }, RECORD_TIME_MS);
 		}
-		recorder();
+//		recorder();
 	}, logToConsoleFunc("requestFileSystem"));
-}, logToConsoleFunc("deviceready"));
+}, false);
